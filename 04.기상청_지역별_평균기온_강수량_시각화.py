d3df894c603aec2fb72bcda321858922a3eaa190
mhlee <mkeasy@gmail.com>
2022-07-26 17:31:48 +0900

    04. 기상청 지역별 평균기온 및 강수량 시각화(응용)

---
+++ 04.기상청_지역별_평균기온_강수량_시각화.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# %%
+from encodings.utf_8 import encode
+from matplotlib import markers
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+
+# 한글화 작업
+plt.figure(dpi=600) # 그래프를 선명하게
+plt.rc('font', family = 'NanumGothic') # 시스템에 폰트설치후, 시스템 재시작
+plt.rc('axes', unicode_minus = False) # 한글 폰트 사용시 마이너스 폰트가 깨지는 문제 해결
+plt.style.use('fivethirtyeight') # 스타일을 사용해 봅니다.
+
+# %%
+# 기상청 지역별 평균기온 및 강수량 시각화
+from datetime import datetime, timedelta
+import pandas as pd
+import requests as r
+import json
+
+serviceKey='clzRha7FjiQHb9pLNqKTq1ieuSzvgbh+gIOGlrwUxQsVVk+fSJD5n5Ggu0YO3RDZEQowJ6eVgvZ65Hrw1C/+Fw=='
+pageNo=1
+numOfRows=31
+startDt='20220701'
+endDt='20220730'
+stnIds=129 # 서산(129), 인천(112),
+
+# 날짜 유효성 검사 및 Fix
+first_day = datetime.today().strftime('%Y%m01')
+yesterday = datetime.today() - timedelta(days=1)
+yesterday = yesterday.strftime('%Y%m%d')
+
+if startDt > yesterday:
+  startDt = first_day
+
+if endDt > yesterday:
+  endDt = yesterday
+
+# %%
+url = 'http://apis.data.go.kr/1360000/AsosDalyInfoService/getWthrDataList'
+params ={'serviceKey' : serviceKey, 'pageNo' : pageNo, 'numOfRows' : numOfRows, 'dataType' : 'JSON', 'dataCd' : 'ASOS', 'dateCd' : 'DAY', 'startDt' : startDt, 'endDt' : endDt, 'stnIds' : stnIds  }
+
+try:
+  res = r.get(url, params=params)
+  s = res.content.decode('utf-8')
+  dict = json.loads(s)
+  result_msg = dict.get('response').get('header').get('resultMsg')
+  if result_msg != 'NORMAL_SERVICE':
+    raise Exception(result_msg)
+
+  item = dict.get('response').get('body').get('items').get('item')
+except Exception as e:
+  print(e)
+
+# %%
+# minTa: 최저 기온 , maxTa: 최고 기온 , sumRn: 일강수량
+df = pd.DataFrame(item)
+df.set_index('tm', inplace=True) # tm: 일시
+df.rename(columns={'avgTa':'평균 기온', 'minTa': '최저 기온', 'maxTa': '최고 기온', 'sumRn': '일강수량'}, inplace=True)
+df.index.name = '일자'
+df = df.replace(r'^\s*$', np.nan, regex=True) # 공백제거
+df['일강수량'] = df['일강수량'].fillna(0.0) # 결측치 초기화
+
+# 타입 변환
+df['일강수량'] = df['일강수량'].astype(float)
+df['평균 기온'] = df['평균 기온'].astype(float)
+df['최고 기온'] = df['최고 기온'].astype(float)
+df['최저 기온'] = df['최저 기온'].astype(float)
+
+# 날짜 추출
+dt = pd.to_datetime(df.index)
+df['년도'] = dt.year
+df['월'] = dt.month
+df['일'] = dt.day
+
+# 중간 10개의 온도 추출
+all_temps = df['평균 기온'].values.astype(float)
+max_temps = df.sort_values(by='평균 기온', ascending=False).head(10)['평균 기온'].to_list()
+min_temps = df.sort_values(by='평균 기온', ascending=True).head(10)['평균 기온'].to_list()
+
+temps = pd.Series(all_temps)
+for value in max_temps:
+    temps.drop(temps[temps == value].index, inplace=True)
+for value in min_temps:
+    temps.drop(temps[temps == value].index, inplace=True)
+
+# 중간값중 중복값 제거
+temps = temps.drop_duplicates()
+temp_max = temps.max()
+temp_min = temps.min()
+
+# 지역코드 정의
+df_city = pd.read_csv('data/city.csv', encoding='utf-8')
+df_city.set_index('id', inplace=True)
+city_name = df_city.loc[stnIds]['name']
+
+# X축을 공유하는 이중 라인 차트 생성
+fig, ax1 = plt.subplots(figsize=(10,6), sharex='col')
+title=f"{city_name} 평균기온 및 일강수량 ({startDt} ~ {endDt})"
+fig.suptitle(title)
+p1 = ax1.plot(df.index, df['평균 기온'], color='#ff6600', marker='o', markersize=10, linewidth=3, markeredgecolor='#ffffff', markeredgewidth=2, alpha=0.8, label='평균기온')
+ax1.set_ylabel('평균 기온(°C)')
+ax1.set_xticklabels(df['일'], ha='center', rotation=0)
+ax1.legend(loc=(0.05, 0.85))
+for idx, val in enumerate(df['평균 기온']):
+  if val >= temp_min and val <= temp_max: # 중간 기온만 출력 (강수량과 겹침 방지)
+    ax1.text(idx, val, str(val), ha='center', va='bottom')
+
+ax2 = ax1.twinx() # x축을 공유하는 축을 생성
+p2 = ax2.bar(df.index, df['일강수량'], color='#0033ff', alpha=0.5, label='일강수량')
+ax2.set_ylabel('일강수량(mm)')
+ax2.legend(loc=(0.05, 0.80))
+for idx, val in enumerate(df['일강수량']):
+  if val >= 1.0: # 1mm 이상만 출력
+    ax2.text(idx, val+0.05, str(val), ha='center', va='bottom')
+
+# 그래프 저장
+fig.savefig('data/'+title+'.png', dpi=300)
