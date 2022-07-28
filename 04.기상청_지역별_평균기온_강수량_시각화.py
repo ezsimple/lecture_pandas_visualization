d8dae9ebab5880e1901dcac3b8d988a0c28469b6
mhlee <mkeasy@gmail.com>
2022-07-28 09:41:24 +0900

    최고, 최저 기온 그래프 추가

--- 04.기상청_지역별_평균기온_강수량_시각화.py
+++ 04.기상청_지역별_평균기온_강수량_시각화.py
@@ -3,6 +3,7 @@
 
 # %%
 from encodings.utf_8 import encode
+from turtle import position
 from matplotlib import markers
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -29,7 +30,7 @@ numOfRows=31
 startDt='20220601'
 endDt='20220630'
 stnIds=112 # 서산(129), 인천(112), 서울(108), 홍천(212)
-# 당진 <-> 대전, 부여, 보령, 서산, 금산, 천안
+# 당진 <-> 대전(125), 부여(113), 보령(83), 서산(31), 금산(135), 천안(51) km 이내
 
 # 날짜 유효성 검사 및 Fix
 today = datetime.today()
@@ -119,10 +120,15 @@ city_name = df_city.loc[stnIds]['name']
 fig, ax1 = plt.subplots(figsize=(10,6), sharex='col')
 title=f"{city_name} 평균기온 및 일강수량 ({startDt} ~ {endDt})"
 fig.suptitle(title)
-p1 = ax1.plot(df.index, df['평균 기온'], color='#ff6600', marker='o', markersize=10, linewidth=3, markeredgecolor='#ffffff', markeredgewidth=2, alpha=0.8, label='평균기온')
-ax1.set_ylabel('평균 기온(°C)')
+ax1.plot(df.index, df['최고 기온'], color='#ff6600', marker='^', ls=':' ,markersize=10, linewidth=1, markeredgecolor='#ffffff', markeredgewidth=2, label='최고기온')
+ax1.plot(df.index, df['평균 기온'], color='#ffc6c0', marker='o', ls='-' ,markersize=10, linewidth=3, markeredgecolor='#ffffff', markeredgewidth=2, label='평균기온')
+ax1.plot(df.index, df['최저 기온'], color='#0066cc', marker='v', ls=':' ,markersize=10, linewidth=1, markeredgecolor='#ffffff', markeredgewidth=2, label='최저기온')
+
+ax1.set_ylabel('최고/평균/최저 기온(°C)')
+ax1.yaxis.set_label_coords(-0.07, 0.5)
+
 ax1.set_xticklabels(df['일'], ha='center', rotation=0)
-ax1.legend(loc=(0.05, 0.85))
+ax1.legend(loc=(1.1, 0.80))
 for idx, val in enumerate(df['평균 기온']):
   if val >= max_temp: #
     ax1.text(idx, val, str(val), ha='center', va='bottom')
@@ -138,7 +144,7 @@ ax1.set_xlabel('과거 일자별 날씨정보 (공공 데이터 이용 자료)')
 ax2 = ax1.twinx() # x축을 공유하는 축을 생성
 p2 = ax2.bar(df.index, df['일강수량'], color='#0033ff', alpha=0.5, label='일강수량')
 ax2.set_ylabel('일강수량(mm)')
-ax2.legend(loc=(0.05, 0.80))
+ax2.legend(loc=(1.1, 0.74))
 for idx, val in enumerate(df['일강수량']):
   if val >= 1.0: # 1mm 이상만 출력
     ax2.text(idx, val+0.05, str(val), ha='center', va='bottom')
